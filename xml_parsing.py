b4081767c2d2607763397bde8c262869256b94f9
Polina Nikitich <polina-nikitich@yandex.ru>
2021-08-25 15:09:21 +1000

    поправила снова

--- xml_parsing.py
+++ xml_parsing.py
@@ -79,27 +79,27 @@ points = list(modules.unique(points))
 # getting points only inside polygon
 inside_points = list(modules.inside(points))
 
-# finding all routes
-routes = list(modules.get_routes(text))
-mvl = list(modules.get_route_info(routes))
-filtred_routes = list(modules.select_routes(mvl, inside_points))
-filtred_routes = list(modules.unique(filtred_routes))
-filtred_routes = sorted(filtred_routes, key=lambda x: x[0])
-filtred_routes = [list(g) for k, g in groupby(filtred_routes, lambda s: s[0])]
-
-# selecting routes with more than 2 points on them
-more2 = list(modules.count_list(filtred_routes))
-more2 = list(chain.from_iterable(more2))
-more2 = list(modules.counter_of_points(more2))
-only_in_trass_points = modules.only_in_trass(more2)
-only_in_trass_points = list(modules.unique(only_in_trass_points))
-names_trass_points = list(modules.names(only_in_trass_points))
-
-# getting points in filtered routes
-filtred_inside_points = [item for item in inside_points if item[0] in names_trass_points]
+# # finding all routes
+# routes = list(modules.get_routes(text))
+# mvl = list(modules.get_route_info(routes))
+# filtred_routes = list(modules.select_routes(mvl, inside_points))
+# filtred_routes = list(modules.unique(filtred_routes))
+# filtred_routes = sorted(filtred_routes, key=lambda x: x[0])
+# filtred_routes = [list(g) for k, g in groupby(filtred_routes, lambda s: s[0])]
+#
+# # selecting routes with more than 2 points on them
+# more2 = list(modules.count_list(filtred_routes))
+# more2 = list(chain.from_iterable(more2))
+# more2 = list(modules.counter_of_points(more2))
+# only_in_trass_points = modules.only_in_trass(more2)
+# only_in_trass_points = list(modules.unique(only_in_trass_points))
+# names_trass_points = list(modules.names(only_in_trass_points))
+#
+# # getting points in filtered routes
+# filtred_inside_points = [item for item in inside_points if item[0] in names_trass_points]
 
 # transform coordinates from gradus to radians
-rad_points = list(modules.radians(filtred_inside_points))
+rad_points = list(modules.radians(inside_points))
 
 # transform coordinates for xls file
 # xls_point = list(modules.gradus(filtred_inside_points))
@@ -249,9 +249,9 @@ class FamiliarNames:
         self.lst = lst
         version = lst[0]
         code = lst[1]
-        code_lat = transliterate.translit(lst[1], 'ru', reversed=True) if lst[2] is None and len(lst[1]) <= 5 else ""
+        code_lat = transliterate.translit(lst[1], 'ru', reversed=True)
         name = lst[1] if lst[3] is None else lst[3]
-        name_lat = transliterate.translit(lst[1], 'ru', reversed=True) if lst[4] is None and len(lst[1]) <= 5 else ""
+        name_lat = transliterate.translit(lst[1], 'ru', reversed=True) if lst[4] is None else ""
         magnetic = 0 if lst[5] is None else lst[5]
         type = "" if lst[6] is None else lst[6]
         if "ь" in code or "Ь" in code:
@@ -269,76 +269,75 @@ class FamiliarNames:
         class_ = "Unknown" if lst[12] is None else lst[12]
         CallLetter = "" if lst[13] is None else lst[13]
         self.data0 = '<?xml version="1.0" encoding="utf-16"?>\n'
-        self.data1 = '<ArrayOfMapPoint xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n'
-        self.data2 = '\t<MapPoint Version="%s" IsDeleted="false">\n' % version
-        self.data3 = '\t\t<ObjectId>%s</ObjectId>\n' % num
-        self.data4 = '\t\t<Id>%s</Id>\n' % num
-        self.data5 = '\t\t<LocalChange>true</LocalChange>\n'
-        self.data6 = '\t\t<LastUpdate>%s</LastUpdate>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        self.data7 = '\t\t<Code>%s</Code>\n' % code
-        self.data8 = '\t\t<CodeLat>%s</CodeLat>\n' % code_lat if code_lat != "" else '\t\t<CodeLat> />\n'
-        self.data9 = '\t\t<Name>%s</Name>\n' % name
-        self.data10 = '\t\t<NameLat>%s</NameLat>\n' % name_lat if name_lat != "" else '\t\t<NameLat />\n'
-        self.data11 = '\t\t<Names />\n'
-        self.data12 = '\t\t<NamesXml />\n'
-        self.data13 = '\t\t<Comment />\n'
-        self.data14 = '\t\t<BeginDate>0001-01-01T00:00:00Z</BeginDate>\n'
-        self.data15 = '\t\t<EndDate>0001-01-01T00:00:00Z</EndDate>\n'
-        self.data16 = '\t\t<ShowOnChart>true</ShowOnChart>\n'
-        self.data17 = '\t\t<Latitude>%s</Latitude>\n' % lat
-        self.data18 = '\t\t<Longitude>%s</Longitude>\n' % lon
-        self.data19 = '\t\t<Elevation>0</Elevation>\n'
-        self.data20 = '\t\t<MagneticDeclination>%s</MagneticDeclination>\n' % magnetic
-        self.data21 = '\t\t<Frequencies />\n'
-        self.data22 = '\t\t<Type>%s</Type>\n' % type
-        self.data23 = '\t\t<SignalCode />\n'
-        self.data24 = '\t\t<IsACP>false</IsACP>\n'
-        self.data25 = '\t\t<IsInOut>false</IsInOut>\n'
-        self.data26 = '\t\t<IsInOutCIS>false</IsInOutCIS>\n'
-        self.data27 = '\t\t<IsGateWay>false</IsGateWay>\n'
-        self.data28 = '\t\t<IsTransferPoint>false</IsTransferPoint>\n'
-        self.data29 = '\t\t<IsTransferPoint_ACP>false</IsTransferPoint_ACP>\n'
-        self.data30 = '\t\t<IsInARZ>false</IsInARZ>\n'
-        self.data31 = '\t\t<IsOutARZ>false</IsOutARZ>\n'
-        self.data32 = '\t\t<IsInRA>false</IsInRA>\n'
-        self.data33 = '\t\t<IsInAirway>false</IsInAirway>\n'
-        self.data34 = '\t\t<IsMvl>true</IsMvl>\n'
-        self.data35 = '\t\t<CorridorNumber />\n'
-        self.data36 = '\t\t<Height>0</Height>\n'
-        self.data37 = '\t\t<HeightAbs>0</HeightAbs>\n'
-        self.data38 = '\t\t<AirportType>%s</AirportType>\n' % airport_type
-        self.data39 = '\t\t<AirportUsageType>%s</AirportUsageType>\n' % AirportUsageType
-        self.data40 = '\t\t<AirportOwnerType>%s</AirportOwnerType>\n' % AirportOwnerType
-        self.data41 = '\t\t<Class>%s</Class>\n' % class_
-        self.data42 = '\t\t<AftnAddr />\n'
-        self.data43 = '\t\t<CallLetter>%s</CallLetter>\n' % CallLetter
-        self.data44 = '\t\t<WorkingTimeRange IsCancelled="false" minlevel="M/M=0/FL=0/FWD" maxlevel="M/M=16100/FL=528/FWD">\n'
-        self.data45 = '\t\t\t<ObjectId>0</ObjectId>\n'
-        self.data46 = '\t\t\t<Id>0</Id>\n'
-        self.data47 = '\t\t\t<IntervalOfClosing>false</IntervalOfClosing>\n'
-        self.data48 = '\t\t\t<Reserv>false</Reserv>\n'
-        self.data49 = '\t\t\t<Kind>Always</Kind>\n'
-        self.data50 = '\t\t\t<Begin>%s</Begin>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        self.data51 = '\t\t\t<End>%s</End>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        self.data52 = '\t\t\t<TimeSpanRanges />\n'
-        self.data53 = '\t\t\t<Comment />\n'
-        self.data54 = '\t\t\t<Sources />\n'
-        self.data55 = '\t\t</WorkingTimeRange>\n'
-        self.data56 = '\t\t<Runways />\n'
-        self.data57 = '\t</MapPoint>\n'
+        self.data2 = """<MapPoint xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" Version="0" IsDeleted="false">\n"""
+        self.data3 = '\t<ObjectId>%s</ObjectId>\n' % num
+        self.data4 = '\t<Id>%s</Id>\n' % num
+        self.data5 = '\t<LocalChange>true</LocalChange>\n'
+        self.data6 = '\t<LastUpdate>%s</LastUpdate>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        self.data7 = '\t<Code>%s</Code>\n' % code
+        self.data8 = '\t<CodeLat>%s</CodeLat>\n' % code_lat if code_lat != "" else '\t\t<CodeLat> />\n'
+        self.data9 = '\t<Name>%s</Name>\n' % name
+        self.data10 = '\t<NameLat>%s</NameLat>\n' % name_lat if name_lat != "" else '\t\t<NameLat />\n'
+        self.data11 = '\t<Names />\n'
+        self.data12 = '\t<NamesXml />\n'
+        self.data13 = '\t<Comment />\n'
+        self.data14 = '\t<BeginDate>0001-01-01T00:00:00</BeginDate>\n'
+        self.data15 = '\t<EndDate>0001-01-01T00:00:00</EndDate>\n'
+        self.data16 = '\t<ShowOnChart>true</ShowOnChart>\n'
+        self.data17 = '\t<Latitude>%s</Latitude>\n' % lat
+        self.data18 = '\t<Longitude>%s</Longitude>\n' % lon
+        self.data19 = '\t<Elevation>0</Elevation>\n'
+        self.data20 = '\t<MagneticDeclination>%s</MagneticDeclination>\n' % magnetic
+        self.data21 = '\t<Frequencies />\n'
+        self.data22 = '\t<Type>%s</Type>\n' % type
+        self.data23 = '\t<SignalCode />\n'
+        self.data24 = '\t<IsACP>false</IsACP>\n'
+        self.data25 = '\t<IsInOut>false</IsInOut>\n'
+        self.data26 = '\t<IsInOutCIS>false</IsInOutCIS>\n'
+        self.data27 = '\t<IsGateWay>false</IsGateWay>\n'
+        self.data28 = '\t<IsTransferPoint>false</IsTransferPoint>\n'
+        self.data29 = '\t<IsTransferPoint_ACP>false</IsTransferPoint_ACP>\n'
+        self.data30 = '\t<IsInARZ>false</IsInARZ>\n'
+        self.data31 = '\t<IsOutARZ>false</IsOutARZ>\n'
+        self.data32 = '\t<IsInRA>false</IsInRA>\n'
+        self.data33 = '\t<IsInAirway>false</IsInAirway>\n'
+        self.data34 = '\t<IsMvl>true</IsMvl>\n'
+        self.data35 = '\t<CorridorNumber />\n'
+        self.data36 = '\t<Height>0</Height>\n'
+        self.data37 = '\t<HeightAbs>0</HeightAbs>\n'
+        self.data38 = '\t<AirportType>%s</AirportType>\n' % airport_type
+        self.data39 = '\t<AirportUsageType>%s</AirportUsageType>\n' % AirportUsageType
+        self.data40 = '\t<AirportOwnerType>%s</AirportOwnerType>\n' % AirportOwnerType
+        self.data41 = '\t<Class>%s</Class>\n' % class_
+        self.data42 = '\t<AftnAddr />\n'
+        self.data43 = '\t<CallLetter>%s</CallLetter>\n' % CallLetter
+        self.data44 = '\t<WorkingTimeRange IsCancelled="false" minlevel="M/M=0/FL=0/FWD" maxlevel="M/M=16100/FL=528/FWD">\n'
+        self.data45 = '\t\t<ObjectId>0</ObjectId>\n'
+        self.data46 = '\t\t<Id>0</Id>\n'
+        self.data47 = '\t\t<IntervalOfClosing>false</IntervalOfClosing>\n'
+        self.data48 = '\t\t<Reserv>false</Reserv>\n'
+        self.data49 = '\t\t<Kind>Always</Kind>\n'
+        self.data50 = '\t\t<Begin>%s</Begin>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        self.data51 = '\t\t<End>%s</End>\n' % datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        self.data52 = '\t\t<TimeSpanRanges />\n'
+        self.data53 = '\t\t<Comment />\n'
+        self.data54 = '\t\t<Sources />\n'
+        self.data55 = '\t</WorkingTimeRange>\n'
+        self.data56 = '\t<Runways />\n'
+        self.data57 = '</MapPoint>'
 
     def change_ID(self):
         self.id = self.lst[14]
         self.ObjectId = self.lst[15] if self.lst[15] != '0' else ""
-        self.data3 = '\t\t<ObjectId>%s</ObjectId>\n' % self.ObjectId
-        self.data4 = '\t\t<Id>%s</Id>\n' % self.id
-        self.data_list = [self.data0, self.data1, self.data2, self.data3, self.data4, self.data5, self.data6,
+        self.data3 = '\t<ObjectId>%s</ObjectId>\n' % self.ObjectId
+        self.data4 = '\t<Id>%s</Id>\n' % self.id
+        self.data_list = [self.data0, self.data2, self.data3, self.data4, self.data5, self.data6,
                           self.data7, self.data8, self.data9, self.data10, self.data11, self.data12, self.data13,
                           self.data14, self.data15, self.data16, self.data17, self.data18, self.data19, self.data20,
                           self.data21, self.data22, self.data23, self.data24, self.data25, self.data26, self.data27,
                           self.data28, self.data29, self.data30, self.data31, self.data32, self.data33, self.data34,
                           self.data35, self.data36, self.data37, self.data38, self.data39, self.data40, self.data41,
-                          self.data41, self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
+                          self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
                           self.data48, self.data49, self.data50, self.data51, self.data52, self.data53, self.data54,
                           self.data55, self.data56, self.data57]
         return self.data_list
@@ -350,19 +349,19 @@ class FamiliarNames:
                           self.data21, self.data22, self.data23, self.data24, self.data25, self.data26, self.data27,
                           self.data28, self.data29, self.data30, self.data31, self.data32, self.data33, self.data34,
                           self.data35, self.data36, self.data37, self.data38, self.data39, self.data40, self.data41,
-                          self.data41, self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
+                          self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
                           self.data48, self.data49, self.data50, self.data51, self.data52, self.data53, self.data54,
                           self.data55, self.data56, self.data57]
         return self.data_list
 
     def simple_list(self):
-        self.data_list = [self.data0, self.data1, self.data2, self.data3, self.data4, self.data5, self.data6,
+        self.data_list = [self.data0, self.data2, self.data3, self.data4, self.data5, self.data6,
                           self.data7, self.data8, self.data9, self.data10, self.data11, self.data12, self.data13,
                           self.data14, self.data15, self.data16, self.data17, self.data18, self.data19, self.data20,
                           self.data21, self.data22, self.data23, self.data24, self.data25, self.data26, self.data27,
                           self.data28, self.data29, self.data30, self.data31, self.data32, self.data33, self.data34,
                           self.data35, self.data36, self.data37, self.data38, self.data39, self.data40, self.data41,
-                          self.data41, self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
+                          self.data42, self.data43, self.data44, self.data45, self.data46, self.data47,
                           self.data48, self.data49, self.data50, self.data51, self.data52, self.data53, self.data54,
                           self.data55, self.data56, self.data57]
         return self.data_list
@@ -418,5 +417,5 @@ for key, value in result_old.items():
 body_query = ',\n'.join(lst_values)
 full_query = begin_query + body_query + ';'
 
-with open('query_old.sql', 'w', encoding='utf-8') as sql:
+with open('query_old.sql', 'w', encoding='cp1251') as sql:
     sql.write(full_query)
